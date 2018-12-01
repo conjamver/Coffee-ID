c585e2f56b7f994dc871eb03a424dd168f248423
Connor Vernon <conjamver@hotmail.com>
2018-12-02 10:28:18 +1100

    Preview image, preview text styling, label styling

--- js/app.js
+++ js/app.js
@@ -10,20 +10,25 @@ methods:{
             //selecting elements and array of images
             var avatarSelect = document.getElementById("avatarSelect");
             var avatar = document.getElementById("avatar");
-            var avatarArray = ["img/ninjaAvatar.png","img/vampireAvatar.png","img/robotAvatar.png", "img/cubeAvatar.png"];
+            var avatarPreview = document.getElementById("avatarPreview");
+            var avatarArray = ["img/cubeAvatar.png", "img/ninjaAvatar.png","img/vampireAvatar.png","img/robotAvatar.png" ];
             //Changing the image 
             switch(avatarSelect.value){
                 case "0":
-                    avatar.src=avatarArray[3];
+                    avatar.src=avatarArray[0];
+                    avatarPreview.src=avatarArray[0];
                     break;
                 case "1":
-                    avatar.src=avatarArray[0];
+                    avatar.src=avatarArray[1];
+                    avatarPreview.src=avatarArray[1];
                     break;
                 case "2":
-                    avatar.src=avatarArray[1];
+                    avatar.src=avatarArray[2];
+                    avatarPreview.src=avatarArray[2];
                     break;
                 case "3":
-                    avatar.src=avatarArray[2];
+                    avatar.src=avatarArray[3];
+                    avatarPreview.src=avatarArray[3];
                     break;
             }
        } 
