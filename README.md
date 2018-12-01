dd33e84342186dfae54939f2e970e4e0e4576e17
Connor Vernon <conjamver@hotmail.com>
2018-12-02 00:42:17 +1100

    Image selector

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Coffee-ID
 Application built using Vue,js to make coffee ID cards. 
+
+Attributions:
+Avatar Icons by: https://game-icons.net under the CC BY 3.0 licence (https://creativecommons.org/licenses/by/3.0/)
+Font icons by Font Awesome. (https://fontawesome.com/license/free)
+
+
--- js/app.js
+++ js/app.js
@@ -3,5 +3,32 @@ var app = new Vue({
   data: {
       firstName:"",
       lastName:""
-  }
-})
+  },
+methods:{
+    //Change Image url FUNCTION
+        AvaterSelect: function(){
+            //selecting elements and array of images
+            var avaterSelect = document.getElementById("avaterSelect");
+            var avater = document.getElementById("avater");
+            var avaterArray = ["../img/ninjaAvater.png","../img/vampireAvater.png","../img/robotAvater.png"];
+            //Changing the image 
+            switch(avaterSelect.value){
+                    
+                case "1":
+                    alert("test");
+                    avatar.src="test.png";
+                    break;
+                case "2":
+                    avatar.src=avaterArray[1];
+                    break;
+                case "3":
+                    avatar.src=avaterArray[2];
+                    break;
+                case "0":
+                    avatar.src="test.png";
+                    break;
+                    
+            }
+       } 
+    }
+});
